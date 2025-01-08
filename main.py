8511de5aefcf537589677f738d02b07291b1a3cb
gsure-tech <Geeescode@>
2025-01-09 00:28:57 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import pandas
+
+
+st.set_page_config(layout="wide")
+
+st.title("The Best Company")
+content = """
+   Hi, I am Abdulganiyu! I am a Software Engineer, teacher and the founder of GsureTech. 
+    I graduated in 2019 with a Bachelor Degree in Computer Science from Bayero University Kano in Nigeria. 
+    I have worked with companies from various states, 
+    such as Maritime Academy as a System Analyst, Hardcore Biometrics as a Software Engineer
+       Hi, I am Abdulganiyu! I am a Software Engineer, teacher and the founder of GsureTech. 
+    
+"""
+st.write(content)
+
+st.header("Our Team")
